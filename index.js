1027c9c56fa263fe86d5e085fec4c54f500ba53b
Damian Suarez <rdsuarez@gmail.com>
2014-11-04 09:19:26 +0100

    core: make `params` request object

--- index.js
+++ index.js
@@ -22,14 +22,34 @@ module.exports = IOAuth;
  */
 
 function IOAuth(client_id, opts){
-  opts = opts || {};
-
+  // `Client ID` must be defined
   if (!client_id) {
     throw '`client_id` is undefined';
   }
-
   debug('client_id: %o', client_id);
 
+  // options
+  opts = opts || {};
+
+  // authentication request params
+  var params = {
+    client_id: client_id,
+    response_type: 'token'
+  };
+
+  // options - `Redirect URL`
+  params.redirect_uri = opts.redirect || location.href.replace(/\#.*$/, '');
+
+  debug('Redirect_URL: %o', params.redirect_uri);
+
+
+  /**
+   * Return implicit oauth function
+   *
+   * @param {Function} fn
+   * @api public
+   */
+  
   return function(fn){
   };
 }
