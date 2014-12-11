0d279dfe75e319b69db6703e0202bed60f0f2094
Damian Suarez <rdsuarez@gmail.com>
2014-12-11 13:37:40 -0300

    test: update testing app

--- test/app.js
+++ test/app.js
@@ -3,15 +3,15 @@
  * Module dependencies.
  */
 
-var iOAuth = require('../')('');  // -< complete your client_id
+var wpOAuth = require('../')('');  // -< complete your client_id
 
-iOAuth.get(function(auth){
+wpOAuth.get(function(auth){
   document.getElementById('token').innerHTML = auth.access_token;
 });
 
 document.getElementById('reset').onclick = function(e) {
-  iOAuth.clean();
-  //iOAuth.request();
+  wpOAuth.clean();
+  //wpOAuth.request();
 };
 
-console.log('-> iOAuth.token() -> ', iOAuth.token());
+console.log('-> wpOAuth.token() -> ', wpOAuth.token());
